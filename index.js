f54784f99f9fa1f72f5ae040e975d0052bc6ae93
Steven Buckvar <stevenbuckvar@Stevens-MBP.fios-router.home>
2018-11-10 15:47:59 -0500

    created webpage and get function

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
 var port = 8361;
 
 // request to get all the parameters. restful API
-app.get("/", function(req, res){
-	res.send("Hello World");
+app.get("/test", function(req, res){
+	console.log('Test API Endpoint Hit!!')
 
 });
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname + "/index.html"));
+});
 // Listen to port
-app.listen(port);
+app.listen(port, function () { console.log('Listening on port ' + port) });
