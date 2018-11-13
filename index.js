ab0a3204968bda79609ce5d515d1e6ef8ecef1d7
Steven Buckvar <stevenbuckvar@Stevens-MBP.fios-router.home>
2018-11-12 22:17:43 -0500

    Added a few responses including fake reddit and the catchAll. Also added ejs file

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var port = 8361;
+var port = 3000;
 
 // request to get all the parameters. restful API
 app.get("/test", function(req, res){
 	console.log('Test API Endpoint Hit!!')
 
+// Fake reddit
+app.get("/r/:subredditName", function(req,res){
+	var subreddit = req.params.subredditName;
+	res.send("Welcome to the " + subreddit + " Subreddit");
+});
+
+
 });
 app.get("/", function(req, res){
 	res.sendFile(path.join(__dirname + "/index.html"));
 });
+
+app.get("*", function(req,res){
+	res.render("catchAll.ejs")
+})
 // Listen to port
 app.listen(port, function () { console.log('Listening on port ' + port) });
