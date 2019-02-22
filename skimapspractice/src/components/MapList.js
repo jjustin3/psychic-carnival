248439d64a603ee411dceae35f68ae124a57544a
stevenbuckvar <sbuckvar@gmail.com>
2019-02-22 10:30:52 -0500

    prepare api request

--- skimapspractice/src/components/MapList.js
+++ skimapspractice/src/components/MapList.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 
-const MapList = () =>{
-    return <div>MapList</div>
+class MapList extends React.Component{
+    onSearchSubmit(term){
+        axios.get('https://skimap.org/Regions/view/', {
+        params: {term}
+        })
+        console.log(term)
+    }
+
+    render(){
+        return <div>hi</div>
+    }
+
 };
 
 export default MapList
