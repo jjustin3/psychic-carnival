e2a077bb053e62e10315048aa1a458afe9f77a41
stevenbuckvar <sbuckvar@gmail.com>
2019-02-22 10:12:13 -0500

    Installed BrowserRouter

--- skimapspractice/src/components/App.js
+++ skimapspractice/src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+import Header from './Header'
+import MapList from './MapList'
 
 const App = () =>{
     return(
-        <div>Hi</div>
+        <div>
+            <BrowserRouter>
+              <div>
+                  <Header/>
+                  <Switch>
+                      <Route path = "/" exact component ={MapList}/>
+                  </Switch>
+              </div>
+            </BrowserRouter>
+        </div>
     )
 };
 
